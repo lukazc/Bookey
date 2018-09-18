4befc9182e8fc456ac60b8396bd690a1c8e4bc61
lukazc <luka.zarkovic.cerneli@gmail.com>
2018-09-18 13:05:26 +0200

    Add product pagination, Move toolbar up to store component root

--- src/app/ui/store/store.component.ts
+++ src/app/ui/store/store.component.ts
@@ -19,6 +19,9 @@ export class StoreComponent implements OnInit {
 
   public selectedCategories: Set<string> = new Set;
 
+  public productsPerPage = 4;
+  public selectedPage = 0;
+
   constructor(private _repository: ProductRepository, breakpointObserver: BreakpointObserver) {
     this.observerSubscription = breakpointObserver.observe([Breakpoints.XSmall])
     .subscribe((result: BreakpointState) => {
@@ -33,18 +36,45 @@ export class StoreComponent implements OnInit {
   ngOnInit() {}
 
 
-  get products(): Product[] {
-    return this._repository.getProducts(this.selectedCategories);
-  }
   get categories(): string[] {
     return this._repository.getCategories();
   }
-
   changeCategory(newCategory?: string) {
     if (this.selectedCategories.has(newCategory)) {
       this.selectedCategories.delete(newCategory);
     } else {
       this.selectedCategories.add(newCategory);
+    }
+  }
+  get productsInCategory(): Product[] {
+    return this._repository.getProducts(this.selectedCategories);
+  }
+  get productsOnPage(): Product[] {
+    const pageIndex = this.selectedPage * this.productsPerPage;
+    const singlePageOfProducts = this.productsInCategory.slice(pageIndex, pageIndex + this.productsPerPage);
+    return singlePageOfProducts;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+  changePageSize(newPageSize: number) {
+    this.productsPerPage = newPageSize;
+    this.changePage(0);
+  }
+  get numberOfPages(): number {
+    return Math.ceil
+    (this.productsInCategory.length / this.productsPerPage);
+  }
+
+  pageEvent(event) {
+    const newPageIndex = event.pageIndex;
+    const newPageSize = event.pageSize;
+    if (this.productsPerPage !== newPageSize) {
+      this.changePageSize(newPageSize);
+    }
+    if (this.selectedPage !== newPageIndex) {
+      this.changePage(newPageIndex);
     }
   }
 }
