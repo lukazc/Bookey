724e9a5b52595c519511285ccf2a387977610e6d
lukazc <luka.zarkovic.cerneli@gmail.com>
2018-10-01 11:52:18 +0200

    Add input form and submit functionality in Checkout component

--- src/app/ui/store/checkout/checkout.component.ts
+++ src/app/ui/store/checkout/checkout.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { Order } from '@app-data/order.model';
+import { OrderRepository } from '@app-data/order.repo';
+import { NgForm } from '@angular/forms';
 
 @Component({
     templateUrl: 'checkout.component.html',
+    styleUrls: ['checkout.component.css'],
     selector: 'app-checkout'
 })
-export class CheckoutComponent {}
+export class CheckoutComponent {
+
+    submitAttempted: boolean = false;
+    orderSent: boolean = false;
+
+    constructor(public repository: OrderRepository, public order: Order) {}
+
+    submitOrder(form: NgForm) {
+        this.submitAttempted = true;
+
+        if (form.valid) {
+            this.repository
+            .saveOrder(this.order)
+            .subscribe(order => {
+                this.order.clear();
+                this.orderSent = true;
+                this.submitAttempted = false;
+            });
+        }
+    }
+}
