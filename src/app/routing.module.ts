d752e2388a46ab69955d568e6ffbd8663a31cc7c
lukazc <luka.zarkovic.cerneli@gmail.com>
2018-12-31 10:01:01 +0100

    Remove Checkout route, Add Cart buttons and change final step icon

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 
-
 import { StoreComponent } from '@app-ui/store/store.component';
 import { CartComponent } from '@app-ui/store/cart/cart.component';
-import { CheckoutComponent } from '@app-ui/store/checkout/checkout.component';
 
 import { StoreFirstGuard } from '@app/route.guard';
 
@@ -17,11 +15,6 @@ const appRoutes: Routes = [
   {
     path: "cart",
     component: CartComponent,
-    canActivate: [StoreFirstGuard]
-  },
-  {
-    path: "checkout",
-    component: CheckoutComponent,
     canActivate: [StoreFirstGuard]
   },
   {
