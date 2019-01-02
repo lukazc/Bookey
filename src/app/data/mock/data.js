29cfd2400671d669a5e5df580f3e13260492b755
lukazc <luka.zarkovic.cerneli@gmail.com>
2019-01-02 09:47:51 +0100

    Update mock data

--- src/app/data/mock/data.js
+++ src/app/data/mock/data.js
@@ -1,25 +1,28 @@
 module.exports = function () {
     return {
         products: [
-            { id: 1, title: "12 Rules for Life", author: "Jordan B. Peterson", category: "Psychology", description: "Learn to live properly", price: 15 },
-            { id: 2, title: "Crime and Punishment", author: "Fyodor Dostoevsky", category: "Philosophical fiction", description: "A desperate young man has some questionable ideas", price: 5 },
-            { id: 3, title: "The Call of Cthulhu", author: "H. P. Lovecraft", category: "Weird horror", description: "A terrifying trilogy of horror", price: 4.99 },
-            { id: 4, title: "The Man Who Mistook His Wife for a Hat and Other Clinical Tales", author: "Oliver Sacks", category: "Medical history", description: "Case histories of some extraordinary patients", price: 21 },
+            { id: 1, title: "12 Rules for Life", author: "Jordan B. Peterson", category: "Psychology", description: "Learn to live properly.", price: 15 },
+            { id: 2, title: "Crime and Punishment", author: "Fyodor Dostoevsky", category: "Philosophical fiction", description: "A desperate young man has some questionable ideas.", price: 5 },
+            { id: 3, title: "The Call of Cthulhu", author: "H. P. Lovecraft", category: "Weird horror", description: "A terrifying trilogy of horror.", price: 4.99 },
+            { id: 4, title: "The Man Who Mistook His Wife for a Hat and Other Clinical Tales", author: "Oliver Sacks", category: "Medical history", description: "Case histories of some extraordinary patients.", price: 21 },
             { id: 5, title: "Harry Potter Box Set", author: "J.K. Rowling", category: "Fantasy fiction", description: "A series of fantasy novels chronicling the life of a young wizard.", price: 50 },
             { id: 6, title: "Code Complete: A Practical Handbook of Software Construction", author: "Steve McConnell", category: "Software development", description: "A compendium of software construction techniques.", price: 30 },
             { id: 7, title: "Three-body Problem", author: "Liu Cixin", category: "Science fiction", description: "A secret military project sends signals into space to establish contact with aliens.", price: 22.50 },
             { id: 8, title: "Permutation City", author: "Greg Egan", category: "Science fiction", description: "Explores quantum ontology through various philosophical aspects of artificial life and simulated reality.", price: 19.99 },
             { id: 9, title: "Thinking: Fast and Slow", author: "Daniel Kahneman", category: "Psychology", description: "Summary of 50 years of psychological experiments.", price: 21.75 },
-            { id: 10, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 11, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 12, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 13, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 14, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 15, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 16, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 17, title: "Placeholder", author: "Place Holder", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 18, title: "Placeholder", author: "Place Holder, Another Author, Yeti Dudes", category: "Place", description: "Pla Ce Hold Er", price: 100 },
-            { id: 19, title: "Placeholder", author: "Place Holder, Another Name, Nicky Tock, Justin Case, Ann Other", category: "Place", description: "Pla Ce Hold Er", price: 100 }
+            { id: 10, title: "Factfulness", author: "Anna Rosling Rönnlund, Hans Rosling, Ola Rosling", category: "Psychology", description: "Ten reasons we're wrong about the world - and why things are better than you think.", price: 27.99 },
+            { id: 11, title: "Deep Work", author: "Cal Newport", category: "Psychology", description: "How to quickly master complicated information and produce better results in less time.", price: 28.00 },
+            { id: 12, title: "Can't Hurt Me", author: "David Goggins", category: "Biography", description: "A story of transformation through self-discipline, mental toughness, and hard work.", price: 24.99 },
+            { id: 13, title: "The Five Invitations", author: "Frank Ostaseski", category: "Psychology", description: "A renowned teacher of compassionate care-giving and the cofounder of the Zen Hospice Project, Frank Ostaseski, distills the lessons gleaned over the course of his career.", price: 15.38 },
+            { id: 14, title: "The Elephant in the Brain", author: "Kevin Simler, Robin Hanson", category: "Psychology", description: "Humans may be self-interested schemers, but we benefit by pretending otherwise.", price: 29.33 },
+            { id: 15, title: "Waking Up", author: "Sam Harris", category: "Spirituality", description: "A guide to meditation as a rational spiritual practice.", price: 10.87 },
+            { id: 16, title: "21 Lessons for the 21st Century", author: "Yuval Noah Harari", category: "History", description: "A probing and visionary investigation into today's most urgent issues as we move into the uncharted territory of the future.", price: 26.25},
+            { id: 17, title: "A Confederacy of Dunces", author: "John Kennedy Toole", category: "Comedy", description: "Ignatius J. Reilly is huge, obese, fractious, fastidious, a latter-day Gargantua, a Don Quixote of the French Quarter.", price: 10.87 },
+            { id: 18, title: "Antifragile", author: "Nassim Nicholas Taleb", category: "Economics", description: "An investigation of opacity, luck, uncertainty, probability, human error, risk, and decision-making.", price: 14.40 },
+            { id: 19, title: "Moral Origins", author: "Christopher Boehm", category: "Psychology", description: "A groundbreaking exploration of the evolution of human generosity and cooperation.", price: 29.31 },
+            { id: 19, title: "The Sun Also Rises", author: "Ernest Hemingway", category: "Fiction", description: "American and British expatriates travel from Paris to the Festival of San Fermín in Pamplona to watch the running of the bulls and the bullfights.", price: 16.00 },
+            { id: 19, title: "The Vital Question", author: "Nick Lane", category: "Popular science", description: "Nick Lane radically reframes evolutionary history, putting forward a solution to conundrums that have puzzled generations of scientists.", price: 16.95 },
+            { id: 19, title: "Enlightenment Now", author: "Steven Pinker", category: "Social philosophy", description: "People are living longer, healthier, freer, and happier lives, and while our problems are formidable, the solutions lie in the Enlightenment ideal of using reason and science.", price: 23.79 }
         ],
         orders: []
     };
