8d63188e3f8b40a4508cc21d442287a9a68e8307
lukazc <luka.zarkovic.cerneli@gmail.com>
2019-02-04 11:23:34 +0100

    Create feedback form

--- src/app/ui/misc/contact/contact.component.ts
+++ src/app/ui/misc/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Feedback } from '@app-data/feedback.model';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  public feedback: Feedback = {};
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitFeedback(form) {
+    if (form.valid) {
+      alert(JSON.stringify(this.feedback));
+    }
   }
 
 }
--- src/app/ui/misc/misc.module.ts
+++ src/app/ui/misc/misc.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialDesignModule } from '@app-ui/material.design.module';
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MaterialDesignModule
   ]
 })
 export class MiscModule { }
